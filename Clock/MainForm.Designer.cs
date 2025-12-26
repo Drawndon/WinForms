ce4c3480f148dd5c5e066c08ec8b412f8b565b39
Андрей Полищук <dcnfandrey@mail.ru>
2025-12-26 19:58:51 +0300

    FontDialog almost DONE

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -152,6 +152,7 @@
 			this.tsmiFont.Name = "tsmiFont";
 			this.tsmiFont.Size = new System.Drawing.Size(240, 32);
 			this.tsmiFont.Text = "Font";
+			this.tsmiFont.Click += new System.EventHandler(this.tsmiFont_Click);
 			// 
 			// tsmiColor
 			// 
